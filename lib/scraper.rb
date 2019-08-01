0b7b1267b14455acc7a299ffaa286a9f253f9965
Emily Lillie <emilylillie3@gmail.com>
2019-08-01 20:29:55 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -3,5 +3,5 @@ require 'open-uri'
 
 html = open("https://flatironschool.com/")
 
-doc = Nokogiri::HTML(html) #putting the html into nested nodes 
+doc = Nokogiri::HTML(open("https://flatironschool.com/")doc.css(".site-header__hero__headline").text.strip 
 
