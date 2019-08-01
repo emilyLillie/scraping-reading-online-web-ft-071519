9bf52ca54f37c3d42f46c713df79eb7224588748
Emily Lillie <emilylillie3@gmail.com>
2019-08-01 20:32:03 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -3,5 +3,7 @@ require 'open-uri'
 
 html = open("https://flatironschool.com/")
 
-doc = Nokogiri::HTML(open("https://flatironschool.com/")doc.css(".site-header__hero__headline").text.strip 
+doc = Nokogiri::HTML(html)
+
+doc.css(".site-header__hero__headline").text.strip 
 
